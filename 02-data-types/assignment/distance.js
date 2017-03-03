3d1db8e1da8b1b405c903277d8af3592e3131150
YOUR FULL NAME <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-03-02 19:08:17 -0500

    Distance formula hw

---
+++ 02-data-types/assignment/distance.js
@@ -0,0 +1,19 @@
+// part 1
+var x1 = 1
+var x2 = 10
+var y1 = 3
+var y2 = 15
+
+// part 2
+var deltaX = x2 - x1
+var deltaY = y2 - y1
+
+// part 3
+var resultX = Math.pow(deltaX, 2)
+var resultY = Math.pow(deltaY, 2)
+
+// part 4
+var result = resultX + resultY
+
+// part 5
+console.log(Math.sqrt(result)) //made corrections here.
